684938932bcf34c85ff23966a633c85579f0259b
Brandon Eng <bce25@cornell.edu>
2020-08-29 10:52:19 -0700

    Updates

---
+++ precache-manifest.58419bfbb80b8900135959809cfcd9f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45e78bcb6c112f6933189a16a8147e3",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "aafc31308c34661f70f8",
+    "url": "/TicTacToe/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "cd369ee5934ceb47d457",
+    "url": "/TicTacToe/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aafc31308c34661f70f8",
+    "url": "/TicTacToe/static/js/2.ad3f69f5.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/TicTacToe/static/js/2.ad3f69f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd369ee5934ceb47d457",
+    "url": "/TicTacToe/static/js/main.c9b1bf50.chunk.js"
+  },
+  {
+    "revision": "35d19f0c13e4574912e8",
+    "url": "/TicTacToe/static/js/runtime-main.29f4516c.js"
+  }
+]);
